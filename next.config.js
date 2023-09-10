4ab53950e36c1469c8c13fa15610e2dc2bfd8c81
Bilal Karakollu <karakollubilal@gmail.com>
2023-09-10 19:45:53 +0300

    feat: change next.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require("./next-i18next.config");
+const path = require("path");
 
 const nextConfig = {
+  debug: process.env.NODE_ENV === "development",
   i18n,
-  reactStrictMode: true
-}
+  reactStrictMode: true,
+  localePath: path.resolve("./public/locales"),
+  reloadOnPrerender: process.env.NODE_ENV === "development",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
