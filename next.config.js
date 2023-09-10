20a5d643968c3f7020f5cb94252be5d988b28a2d
Bilal Karakollu <karakollubilal@gmail.com>
2023-09-10 22:28:18 +0300

    feat: update conf

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
-/** @type {import('next').NextConfig} */
 const { i18n } = require("./next-i18next.config");
+const { loadEnvConfig } = require("@next/env");
+
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
+
+/** @type {import('next').NextConfig} */
 
 const nextConfig = {
   i18n,
-  reactStrictMode: true,
+  trailingSlash: true,
+  generateEtags: false,
+  poweredByHeader: false,
+  compress: false,
 };
 
 module.exports = nextConfig;
--- server.js
+++ server.js
@@ -1,40 +1,53 @@
-const { createServer } = require("http");
-const { parse } = require("url");
-const next = require("next");
+/* eslint-disable @typescript-eslint/no-var-requires */
+require('events').EventEmitter.defaultMaxListeners = Infinity
+const http = require('http')
+const { parse } = require('url')
 
-const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
-const port = 3000;
-// when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port });
-const handle = app.getRequestHandler();
+const next = require('next')
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+const forceTimeout = 60000
 
 app.prepare().then(() => {
-  createServer(async (req, res) => {
-    try {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
-      const parsedUrl = parse(req.url, true);
-      const { pathname, query } = parsedUrl;
+  let isAppGoingToBeClosed = false
 
-      if (pathname === "/a") {
-        await app.render(req, res, "/a", query);
-      } else if (pathname === "/b") {
-        await app.render(req, res, "/b", query);
-      } else {
-        await handle(req, res, parsedUrl);
+  // TODO: Create documentation for creating secure certificate and use it here
+  const server = http
+    .createServer((req, res) => {
+      const parsedUrl = parse(req.url, true)
+
+      if (isAppGoingToBeClosed) {
+        res.setHeader('Connection', 'close')
       }
-    } catch (err) {
-      console.error("Error occurred handling", req.url, err);
-      res.statusCode = 500;
-      res.end("internal server error");
-    }
-  })
-    .once("error", (err) => {
-      console.error(err);
-      process.exit(1);
+
+      handle(req, res, parsedUrl)
     })
-    .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`);
-    });
-});
+    .listen(3000, (err) => {
+      if (err) throw err
+      console.log('> Ready on http://localhost:3000')
+
+      if (process.send) {
+        process.send('ready')
+      }
+    })
+
+  function gracefulShutdown() {
+    console.log('Either SIGINT or SIGTERM received. Try shutdown gracefully...')
+
+    isAppGoingToBeClosed = true
+
+    server.close(() => {
+      console.log('Close remaining connections')
+      process.exit()
+    })
+
+    setTimeout(() => {
+      console.log('Force shutdown cause cannot close all connections after 1 mins')
+      process.exit()
+    }, forceTimeout)
+  }
+
+  process.on('SIGTERM', gracefulShutdown)
+  process.on('SIGINT', gracefulShutdown)
+})
