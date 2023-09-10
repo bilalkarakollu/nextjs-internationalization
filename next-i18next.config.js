bb1eeb705f2a1ad1f0974bedb0b32bc200987290
Bilal Karakollu <karakollubilal@gmail.com>
2023-09-10 22:47:33 +0300

    feat: change conf

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,11 @@
+const path = require("path");
 module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"],
     localeDetection: false,
+    localePath: path.resolve("./public/locales"),
+    localeStructure: "{{lng}}/{{ns}}",
   },
   lowerCaseLng: true,
 };
--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 const { i18n } = require("./next-i18next.config");
-const path = require("path");
 
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
   i18n,
-  localePath: path.resolve("./public/locales"),
-  localeStructure: "{{lng}}/{{ns}}",
 };
 
 module.exports = nextConfig;
